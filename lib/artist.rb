8bbfc93f71911425a082455c54f85d515a792204
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-30 03:13:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,7 @@
-require_relative '../lib/concerns/memorable'
-
-class Artist
-  attr_accessor :name
-  attr_reader :songs
-  
-  extend Memorable
-  extend Findable
-
-  @@artists = []
-
-  def initialize
-    @@artists << self
-    @songs = []
-  end
-
-  def self.all
-    @@artists
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+module Paramable  
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
   end
 end
