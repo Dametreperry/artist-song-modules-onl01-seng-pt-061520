b3870ec5ba66d03f1f012563fcd33868b746c5e3
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-30 03:14:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,14 @@
-require_relative '../lib/concerns/memorable'
-
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
-  
-  extend Memorable
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
@@ -18,9 +16,6 @@ class Song
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 end
