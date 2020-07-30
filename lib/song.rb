a3ab15128d883865a761bdd6346b829c4a612a26
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-30 03:30:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,19 @@
 class Song
-
+  
   extend Memorable::ClassMethods
-  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def self.all
-    @@songs
-  end
-
   def artist=(artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self)
+  end
+
+  def self.all
+    @@songs
   end
 end
